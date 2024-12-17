26f58abfc769e0400e9cde12cd48faa9d43854b4
vtalos <v.talos23@gmail.com>
2024-12-17 22:01:49 +0200

    Define theme

--- app/src/main/java/com/example/estiaseek/ui/theme/Theme.kt
+++ app/src/main/java/com/example/estiaseek/ui/theme/Theme.kt
@@ -16,32 +16,23 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = estiaSeekRedDark,
+    secondary = estiaSeekRedComplimentDark,
+    tertiary = estiaSeekRedComplimentDark
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = estiaSeekRed,
+    secondary = estiaSeekRedCompliment,
+    tertiary = estiaSeekRedCompliment
 )
 
 @Composable
 fun EstiaSeekTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    // Set to 'false' to enforce consistent custom branding colors.
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
