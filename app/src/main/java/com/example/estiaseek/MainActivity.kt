da8eaec906a338e16a7509b94808dd1f53cb141d
simosathan9 <simosathan03@gmail.com>
2025-01-11 18:07:33 +0200

    Fix upload size issue (#22)

--- app/src/main/java/com/example/estiaseek/MainActivity.kt
+++ app/src/main/java/com/example/estiaseek/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.estiaseek
 
+import android.annotation.SuppressLint
+import android.database.CursorWindow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,14 +13,24 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.estiaseek.data.EstiaSeekDatabase
 import com.example.estiaseek.data.OfflineUsersRepository
 import com.example.estiaseek.ui.navigation.NavigationHelper
+import com.example.estiaseek.ui.theme.EstiaSeekTheme
 import com.example.estiaseek.ui.viewmodels.CandidateSearchViewModel
 import com.example.estiaseek.ui.viewmodels.CandidateSearchViewModelFactory
 import com.example.estiaseek.ui.viewmodels.CreateApplicantViewModel
-import com.example.estiaseek.ui.theme.EstiaSeekTheme
+import java.lang.reflect.Field
+
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("DiscouragedPrivateApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        try {
+            val field: Field = CursorWindow::class.java.getDeclaredField("sCursorWindowSize")
+            field.isAccessible = true
+            field.set(null, 100 * 1024 * 1024) //the 100MB is the new size
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
         // Initialize the database and repository
         val database = EstiaSeekDatabase.getDatabase(applicationContext)
